edc68a9debe683a660bec25e3357bb5eafd13f0d
André König <andre.koenig@gmail.com>
2014-05-19 09:19:36 +0200

    Removed 'imperial' switch.

--- advanced.js
+++ advanced.js
@@ -4,8 +4,7 @@ var distance = require('google-distance');
 
 var options = {
     origin: '40.759011,-73.984472',
-    destination: '37.810848,-122.267448',
-    units: 'imperial'
+    destination: '37.810848,-122.267448'
 };
 
 function onDistance (err, result) {
